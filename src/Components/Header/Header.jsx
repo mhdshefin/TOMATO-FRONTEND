a015aa5d0e2a6fb3bcb000d247001e46fe19e1ce
mhdshefin <rishnabinthsalam@gmail.com>
2025-01-16 19:27:48 +0530

    header updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
+    const [windowLength, setWindowLength] = useState(window.innerWidth)
+
+    useEffect(()=>{
+     setWindowLength(window.innerWidth)   
+    })
     return (
-        <div className='Header' id='header'>
-            <div className="header-contents">
-                <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingrediants and culinary expertise. Our mission is to satisfy your cravings and elevate your dinning exprerience. One delicious meel at a time </p>
-                <a href="#explore-menu"><button  >View Menu</button></a>
-            </div>
-        </div>
+        <>
+            {windowLength > 450 ? <div className='Header' id='Header'>
+                <div className='header-contents'>
+                    <h2>Order your favourite food here</h2>
+                    <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingrediants and culinary expertise. Our mission is to satisfy your cravings and elevate your dinning exprerience. One delicious meel at a time </p>
+                    <a href="#explore-menu"><button  >View Menu</button></a>
+                </div>
+            </div> :
+                <div className='header-mobile'>
+                    <img src={assets.header_mobile} alt="" />
+                </div>
+            }
+
+        </>
     )
 }
 
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,6 +2,7 @@ import basket_icon from './basket_icon.png'
 import logo from './logo.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
+
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
 import menu_3 from './menu_3.png'
@@ -60,6 +61,7 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 import menu from './menu.png'
+import header_mobile from './header_mobile.png'
 
 export const assets = {
     logo,
@@ -77,6 +79,7 @@ export const assets = {
     twitter_icon,
     cross_icon,
     selector_icon,
+    header_mobile,
     profile_icon,
     logout_icon,
     bag_icon,
